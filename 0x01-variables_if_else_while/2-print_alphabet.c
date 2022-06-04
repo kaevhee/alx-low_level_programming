639919ee987d4bbfc7e714f21a6b64a5b959a6f4
kaevhee <kawengpam56@gmail.com>
2022-06-04 04:32:56 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,17 @@
  */
 
 int main(void)
+
 {
-	int l = 'a';
-	while (l <= 'z')
-	{
-		putchar(l);
-		l += 1;
-	}
-	putchar('\n');
-	return (0);
+int l = 'a';
+
+while (l <= 'z')
+
+{
+putchar(l);
+l += 1;
+}
+
+putchar('\n');
+return (0);
 }
